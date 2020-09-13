bc65d27ad5bd0ce7632bcba5ba0b31fcbe4fc113
vanweerden <ando.vanweerden@gmail.com>
2020-09-13 08:35:32 +0800

    Added SQLite module and db file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const sqlite = require('sqlite3').verbose;
 
 const app = express();
 
